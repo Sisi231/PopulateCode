990636e8e466183dd84347ac234790ff3085d02c
s.andreeva23 <s.andreeva23@LABSCIFIPC07>
2023-03-09 09:18:58 +0200

    update1

---
+++ WpfApp1/Window3.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Window3.xaml
+    /// </summary>
+    public partial class Window3 : Window
+    {
+        public Window3()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            string dbsCon = @"Data Source=LABSCIFIPC07\LOCALHOST; Initial Catalog=StudentRecords; Integrated Security=True";
+            SqlConnection sqlCon_ = new SqlConnection(dbsCon);
+
+            try
+            {
+                sqlCon_.Open();
+                string query = "Select Subject_ID,SubjectName, SubjctDescription, Teacher_ID from Subject";
+                SqlCommand cmd = new SqlCommand(query, sqlCon_);
+                cmd.ExecuteNonQuery();
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                DataGrid_.ItemsSource = dt.DefaultView;
+                adapter.Update(dt);
+                MessageBox.Show("Successful loading");
+                sqlCon_.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
